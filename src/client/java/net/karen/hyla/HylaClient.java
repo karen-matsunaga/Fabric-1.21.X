07f8cbd6e967c633e1d704d7638fbd20d675d25e
karen matsunaga <Karen.f.matsunaga@gmail.com>
2025-10-30 09:39:33 -0300

    2025-10-30 Installed Fabric 1.21.X version

---
+++ src/client/java/net/karen/hyla/HylaClient.java
@@ -0,0 +1,10 @@
+package net.karen.hyla;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HylaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
